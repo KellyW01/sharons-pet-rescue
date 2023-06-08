dfcfe8c625919481d2e70acd10e485cceddac2bb
kelly.w <kelly.conant.winn@gmail.com>
2023-06-08 14:08:44 -0400

    Added pets to browser and added click event

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
-const createPet = function(name, species) {
+const createPet = function (name, species) {
     const pet = {
-        name : name,
-        species : species,
-        isTired : 5, //scale from 1 (awake) t0 10 (tired)
+        name: name,
+        species: species,
+        isTired: 5, //scale from 1 (awake) t0 10 (tired)
 
         //method for sleep
-        sleep : function(){
+        sleep: function () {
             console.log(`${this.name} needs a nap. Zzz...`);
             this.isTired = 1;
         },
 
         //method for playtime
-        play : function (){
-            if (this.isTired ===10){
+        play: function () {
+            if (this.isTired === 10) {
                 console.log(`${this.name} is too tired to play.`);
                 this.sleep();
             } else {
                 console.log(`Yay! ${this.name} loves to play!`);
                 this.isTired += 1;
-                }
+            }
         }
     };
-return pet;
+    return pet;
 };
 
 //pets
@@ -34,7 +34,37 @@ const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-clover.sleep();
-baxter.play();
+// clover.sleep();
+// baxter.play();
 
-console.log(clover, baxter);
+// console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+//array of our pets
+const allPets = [
+    sora,
+    clover,
+    baxter,
+    cleo,
+    francine
+];
+
+//display pets in browser
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+    for (let pet of petArray) { //pet var above, let to allow it to change
+        let status = "ready to play!"; //let b/c value will change
+        if (pet.isTired >= 7) {
+            status = "sleeping."
+        }
+        const li = document.createElement("li");
+        li.innerHTML =`<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}.`
+        pets.append(li);
+    }
+};
+//click event
+statusButton.addEventListener("click",function(){
+showPets(allPets);
+});
