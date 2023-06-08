fb93d8d0164ecd32e893f58d2fd6bccc4721d7e6
kelly.w <kelly.conant.winn@gmail.com>
2023-06-08 13:23:29 -0400

    Added pets

--- js/script.js
+++ js/script.js
@@ -5,19 +5,36 @@ const createPet = function(name, species) {
     const pet = {
         name : name,
         species : species,
-        isTired : 5,
+        isTired : 5, //scale from 1 (awake) t0 10 (tired)
+
+        //method for sleep
         sleep : function(){
-            console.log(`${name} needs a nap. Zzz...`);
-            isTired = 1;
+            console.log(`${this.name} needs a nap. Zzz...`);
+            this.isTired = 1;
         },
+
+        //method for playtime
         play : function (){
             if (this.isTired ===10){
-                console.log(`${this.name} is too tired to play`);
+                console.log(`${this.name} is too tired to play.`);
                 this.sleep();
-            } else {console.log(`${this.name} loves to play!`);
-                    isTired +1;
-                    }
-        },
+            } else {
+                console.log(`Yay! ${this.name} loves to play!`);
+                this.isTired += 1;
+                }
+        }
     };
-return pet();
+return pet;
 };
+
+//pets
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
+
+clover.sleep();
+baxter.play();
+
+console.log(clover, baxter);
